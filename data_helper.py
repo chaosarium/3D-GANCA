fcb44c5cbb9d62d65fc3b059c209a76cde32056a
chaosarium <leonluleonlu@gmail.com>
2022-02-08 12:30:05 +0800

    extract code from data notebook to helper funcs

---
+++ data_helper.py
@@ -0,0 +1,66 @@
+import numpy as np
+from einops import rearrange
+import pandas as pd
+from tqdm.notebook import tqdm
+DATA_DIR = "dataset/house_data/houses"
+WORLD_SIZE = 32
+import math
+
+def rearrange_sample(house):
+    # this rearranges the training sample into an x y z m shape
+    world = rearrange(house, 'z y x b -> x y z b')
+    return world
+
+def trim_world_empty(world):
+    # this removes empty voxels sorrounding a world
+    arr_slices = tuple(np.s_[curr_arr.min():curr_arr.max() + 1] for curr_arr in world[:,:,:,0].nonzero())
+    return world[arr_slices]
+
+def pad_world(world):
+    # this pads a world to the smallest cubic volume that it can fit in
+    (x, y, z, m) = world.shape
+    min_dim = max(world.shape[0:3])
+    world = np.pad(world, ((0, min_dim - x),(0, min_dim - y),(0, min_dim - z), (0, 0)), 'constant')
+    return world
+
+def something():
+    return "hi"
+
+def center_and_pad_world(world, min_size=None):
+    world = trim_world_empty(world)
+    # this pads a world to the smallest cubic volume that it can fit in
+    (x, y, z, m) = world.shape
+    if min_size:
+        min_dim = min_size
+    else:
+        min_dim = max(world.shape[0:3])
+    
+    x_missing = min_dim - x
+    y_missing = min_dim - y
+    z_missing = min_dim - z
+    
+    world = np.pad(world, ((math.floor(x_missing/2), math.ceil(x_missing/2)),(math.floor(y_missing/2), math.ceil(y_missing/2)),(math.floor(z_missing/2), math.ceil(z_missing/2)), (0, 0)), 'constant')
+    return world
+
+def get_filtered_stats_df():
+    # load filtered houses
+    filtered_df = pd.read_pickle("dataset/filtered_houses_stats.pkl")
+    return filtered_df
+
+def all_trainx_as_df():
+    # load all training examples
+    filtered_df = get_filtered_stats_df()
+    filtered_houses_df = pd.DataFrame() # new df
+    for i, row in tqdm(filtered_df.iterrows(), total=filtered_df.shape[0]):
+        filtered_houses_df = filtered_houses_df.append({
+            'world': center_and_pad_world(trim_world_empty(rearrange_sample(np.load(DATA_DIR + "/" + row['dir'] + "/schematic.npy"))), WORLD_SIZE),
+            'dir': row['dir'],
+            'Size': row['Size'],
+            'Num unique IDs': row['Num unique IDs'],
+            'Num unique blocks': row['Num unique blocks'],
+            'Percentage air untrimmed': row['Percentage air untrimmed'],
+            'Percentage air trimmed': row['Percentage air trimmed'],
+        }, ignore_index=True)
+
+    print('loaded', len(filtered_houses_df), 'houses')
+    return filtered_houses_df
---
+++ visualise_helper.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import rgb2hex
+import math
+from tqdm.notebook import tqdm
+import numpy as np
+import copy
+
+
+def get_color_dict(unique_vals):
+    state = np.random.RandomState(0)
+    color_arr = list(state.uniform(0, 1, (len(unique_vals), 3)))
+    color_arr = [rgb2hex(color) for color in color_arr]
+    color_arr = [None] + color_arr # set all to 0 for air block (block id 0)
+    colors = color_arr[: len(unique_vals)] # chop off the last random
+    color_dict = {str(unique_vals[i]): colors[i] for i in range(len(unique_vals))} # make string
+    return color_dict # return
+
+def convert_to_color(arr, color_dict):
+    new_arr = copy.deepcopy(arr).astype(object)
+    for k in color_dict:
+        new_arr[new_arr == int(k)] = color_dict[k]
+    return new_arr
+
+def visualise_world(worlds_data, plots_per_row = 4, figsize = (20, 20)):
+    '''
+    worlds_data: a pd dataframe with column 'world' and column 'dir'
+    plots_per_row: how many plots in a row, default 4
+    figsize: default (20, 20)
+    '''
+    
+    worlds = worlds_data['world'].to_list()
+    dirs = worlds_data['dir'].to_list()
+    
+    fontsize = 2 * (figsize[0] / plots_per_row)
+    
+    try:
+        _ = worlds[0][0][0][0][0]
+    except IndexError:
+        worlds = [worlds] 
+
+    fig, axs = plt.subplots(math.ceil(len(worlds)/plots_per_row), plots_per_row, figsize = figsize, subplot_kw={"projection": '3d', "adjustable": 'box'})
+    
+    try:
+        _ = axs[0][0]
+    except:
+        axs = [axs] 
+        
+    r, c = 0, 0
+    for i, world in enumerate(tqdm(worlds)):
+
+        (x, y, z, b) = world.shape
+        blockidarray, blockmetaarray = world[:,:,:,0], world[:,:,:,1]
+
+        color_dict = get_color_dict(np.unique(blockidarray))
+        colors = convert_to_color(blockidarray, color_dict)
+        
+        meta_color_dict = get_color_dict(np.unique(blockmetaarray))
+        edge_colors = convert_to_color(blockmetaarray, meta_color_dict)
+        
+        axs[r][c].set_title(dirs[i], fontsize=fontsize)
+        axs[r][c].voxels(blockidarray, facecolors=colors, edgecolors=edge_colors)
+        c += 1
+        if c == plots_per_row:
+            c = 0
+            r += 1
+
+    plt.show()
